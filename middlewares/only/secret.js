fd883f5ad50978e4edaa12151fcb65dfc11b3779
Void <152130497+i1void@users.noreply.github.com>
2026-03-20 19:17:18 +0700

    .

--- middlewares/only/secret.js
+++ middlewares/only/secret.js
@@ -1,42 +1,23 @@
 export default (req, res, next) => {
    const isGet = req.method?.toLowerCase() === 'get'
-   const validatorFn = isGet ? global.validator.get : global.validator.post
    const source = isGet ? req.query : req.body
 
-   const check = validatorFn(req, ['secret'])
-   if (!check.status) {
-      return res.status(400).json(check)
-   }
+   const check = isGet ? global.validator.get(req, ['secret']) : global.validator.post(req, ['secret'])
+   if (!check.status) return res.status(400).json(check)
 
    for (const paramName in source) {
       if (paramName === 'secret') continue
-
       const value = source[paramName]
       if (value) {
-         let formatCheck = { status: true }
-
-         if (paramName === 'url') {
-            formatCheck = global.validator.url(value)
-         } else if (paramName === 'number') {
-            formatCheck = global.validator.number(value)
-         }
-
-         if (!formatCheck.status) {
-            formatCheck.msg = `Parameter "${paramName}" has an invalid format. (${formatCheck.msg})`
-            return res.status(400).json(formatCheck)
-         }
+         let fc = paramName === 'url' ? global.validator.url(value)
+                : paramName === 'number' ? global.validator.number(value)
+                : { status: true }
+         if (!fc.status) { fc.msg = `Parameter "${paramName}" has an invalid format. (${fc.msg})`; return res.status(400).json(fc) }
       }
    }
 
-   const secret = source.secret
-   console.log(secret)
-   if (secret !== process.env.SITE_SECRET_KEY) {
-      return res.status(403).json({
-         creator: global.creator,
-         status: false,
-         msg: 'Invalid secret key'
-      })
-   }
+   if (source.secret !== process.env.SITE_SECRET_KEY)
+      return res.status(403).json({ creator: global.creator, status: false, msg: 'Invalid secret key' })
 
    next()
 }
