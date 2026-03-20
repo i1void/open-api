d6b514b5406eb5dbd9161e8d971e6e47c95fdc5a
Void <152130497+i1void@users.noreply.github.com>
2026-03-20 19:12:22 +0700

    .

--- routers/index.js
+++ routers/index.js
@@ -2,20 +2,12 @@ export const routes = {
    category: 'main',
    path: '/',
    method: 'get',
-   execution: async (req, res, next) => {
+   execution: async (req, res) => {
       try {
          const json = await (await fetch('http://ip-api.com/json')).json()
-         res.json({
-            creator: global.creator,
-            status: true,
-            data: json
-         })
+         res.json({ creator: global.creator, status: true, data: json })
       } catch (e) {
-         res.json({
-            creator: global.creator,
-            status: false,
-            msg: e.message
-         })
+         res.json({ creator: global.creator, status: false, msg: e.message })
       }
    },
    error: false
